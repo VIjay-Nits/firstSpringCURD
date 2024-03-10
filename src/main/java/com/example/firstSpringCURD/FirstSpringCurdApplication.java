b228b70816ea435b1a44c65b7133119991382ae1
Vijay <vijaygupta131999@gmail.com>
2024-03-10 14:37:46 +0530

    first commit project setup

---
+++ src/main/java/com/example/firstSpringCURD/FirstSpringCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringCURD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringCurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstSpringCURD/FirstSpringCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringCURD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
